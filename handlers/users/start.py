80b8bb63fc6d8a4eaf39bb533c489699a2ad994e
Og'abek Sattorov <ogabek15.00@gmail.com>
2021-11-08 23:52:16 +0500

    initial commit

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -15,7 +15,7 @@ async def bot_start(message: types.Message):
     #except sqlite3.IntegrityError as err:
     #    await bot.send_message(chat_id=ADMINS[0], text=err)
 
-    await message.answer(f"Salom, {message.from_user.full_name}.\nO'zingizga yoqqan fontni tanlang!", reply_markup=fonts_menu)
+    await message.answer(f"Salom, {message.from_user.full_name}\nO'zingizga yoqqan fontni tanlang!", reply_markup=fonts_menu)
 
     #count = db.count_users()[0]
     #msg = f"{message.from_user.full_name} bazaga qo'shildi.\nBazada {count} ta foydalanuvchi bor"
