e57c32c357b68a7d576ec8d0baab1792b325ef88
Og'abek Sattorov <ogabek15.00@gmail.com>
2021-11-08 23:57:11 +0500

    initial commit

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,10 +6,7 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam",
-            "Botdan foydalanish uchun /start komandasini bosing va o'zingizga yoqqan fontni tanlang!",
+    text = ("Botdan foydalanish uchun /start komandasini bosing va o'zingizga yoqqan fontni tanlang!",
             "Botdan foydalanishda, muammoga duch kelsangiz @Ogabek_Sattorov ga murojaat qiling!")
     
     await message.answer("\n".join(text))
