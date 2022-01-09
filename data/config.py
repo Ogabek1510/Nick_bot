c2178743c4b9fb001fb954ab920c1ab5dcb20a47
Og'abek Sattorov <ogabek15.00@gmail.com>
2022-01-09 10:44:47 +0500

    initial commit

--- data/config.py
+++ data/config.py
@@ -15,5 +15,5 @@ import os
 
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
 ADMINS = list(os.environ.get("ADMINS"))
-CHANNELS = list(os.environ.get("channels"))
+CHANNELS = str(os.environ.get("channels"))
 IP = str(os.environ.get("ip"))
