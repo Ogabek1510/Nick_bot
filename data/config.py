157ce3c73b3b18d168014da8eab74cde69e925e5
Og'abek Sattorov <ogabek15.00@gmail.com>
2022-01-09 12:51:45 +0500

    initial commit

--- data/config.py
+++ data/config.py
@@ -14,6 +14,6 @@
 import os
 
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
-ADMINS = list(os.environ.get("ADMINS"))
+ADMINS = list(os.environ.get("admins"))
 IP = str(os.environ.get("ip"))
 CHANNELS = ['@nima_nega_qanday']
