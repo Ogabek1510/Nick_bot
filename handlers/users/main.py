0a4682b46e7ceb6385784dde21d913535edeec41
Og'abek Sattorov <ogabek15.00@gmail.com>
2022-01-12 22:13:00 +0500

    initial commit

--- handlers/users/main.py
+++ handlers/users/main.py
@@ -592,7 +592,7 @@ async def Decor(message: types.Message, state: FSMContext):
 
 @dp.callback_query_handler(text="next", state=Fonts.beauty)
 async def Checker(call: types.CallbackQuery):
-    await call.message.delete()
     for text in nicks:
+        await call.message.delete()
         await call.message.answer(text, reply_markup=next_button)
 
